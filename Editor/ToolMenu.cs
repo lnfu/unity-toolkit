0b5a17746d53fdd2d94a754ae796bebe649bbe9d
Enfu Liao <enfu.liao.cs10@nycu.edu.tw>
2024-01-14 19:12:50 +0800

    feat: Load New Manifest
    - load the manifest.json from github gist
    - automation to install/remove packages

--- Editor/ToolMenu.cs
+++ Editor/ToolMenu.cs
@@ -1,5 +1,8 @@
 using UnityEditor;
 using UnityEngine;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
 using static System.IO.Directory;
 using static System.IO.Path;
 using static UnityEngine.Application;
@@ -12,18 +15,46 @@ namespace lnfu
 	    [MenuItem("Tools/Setup/Create Default Directories")]
 	    public static void CreateDefaultDirectories()
 	    {
-		    Debug.Log("test");
 		    CreateDirectories("_Project", "Scripts", "Art", "Scenes");
 		    Refresh();
 	    }
 
-	    public static void CreateDirectories(string root, params string[] dir)
+	    [MenuItem("Tools/Setup/Load New Manifest")]
+	    public static async void LoadNewManifest()
+	    {
+		    var url = GetGistUrl("5340644135ddc9d074bbe544d03f33f0");
+		    var contents = await GetContents(url);
+		    ReplacePackageFile(contents);
+	    }
+
+	    static void CreateDirectories(string root, params string[] dir)
 	    {
 		    var fullpath = Combine(dataPath, root);
 		    foreach (var newDirectory in dir)
 		    {
 			    CreateDirectory(Combine(fullpath, newDirectory));
 		    }
+	    }
+
+	    // https://gist.githubusercontent.com/lnfu/5340644135ddc9d074bbe544d03f33f0/raw
+	    static string GetGistUrl(string id, string user = "lnfu")
+	    {
+		    return $"https://gist.githubusercontent.com/{user}/{id}/raw";
+	    }
+
+	    static async Task<string> GetContents(string url)
+	    {
+		    using var client = new HttpClient();
+		    var response = await client.GetAsync(url);
+		    var content = await response.Content.ReadAsStringAsync();
+		    return content;
+	    }
+
+	    static void ReplacePackageFile(string contents)
+	    {
+		    var existing = Path.Combine(Application.dataPath, "../Packages/manifest.json");
+		    File.WriteAllText(existing, contents);
+		    UnityEditor.PackageManager.Client.Resolve();
 	    }
     }
 }
